9c4521b8122d57394b3cde55f66e6e8f988761d4
lfolco <me@laurafolco.com>
2026-06-08 17:14:07 -0400

    fix deployment error

--- deployer-hosts.php
+++ deployer-hosts.php
@@ -10,11 +10,11 @@ namespace Deployer;
 host('forger')
     ->setHostname(getenv('SSH_HOST'))
     ->setRemoteUser(getenv('SSH_USER'))
-    ->setPort(getenv('SSH_PORT', 22))
-    ->setDeployPath(getenv('SSH_PATH', '/var/www/deployer'));
+    ->setPort((int)(getenv('SSH_PORT') ?: 22))
+    ->setDeployPath(getenv('SSH_PATH') ?: '/var/www/deployer');
 
 // host does not allow GHA whitelisting so we use a jump box (ssh config built in GHA)
 host('forger-jump')
     ->setHostname('forger-jump')
     ->setRemoteUser(getenv('SSH_USER'))
-    ->setDeployPath(getenv('SSH_PATH', '/var/www/deployer'));
+    ->setDeployPath(getenv('SSH_PATH') ?: '/var/www/deployer');
